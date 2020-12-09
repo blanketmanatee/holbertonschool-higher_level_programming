3117ac4cf1402ef49b820220df0604b36de70fe0
blanketmanatee <ashleyl.welch@gmail.com>
2020-12-09 04:59:05 +0000

    initial push

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+range (1, 100)
+for a:
+
+if a % 3 == 0 and a % 5 == 0:
+
+print("FizzBuzz")
+
+elif a % 3 == 0:
+
+print("Fizz")
+
+else a % 5 == 0:
+
+print("Buzz")
+
