21641f7143ec42a36380f6c88b98fac65059e996
blanketmanatee <ashleyl.welch@gmail.com>
2020-12-09 04:26:54 +0000

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-
-if ord(str) <= 97 and >= 122:
+    for c in str:
+        if ord(c) >= 97 and ord(c)<= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
