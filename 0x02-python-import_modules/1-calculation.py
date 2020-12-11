3f1bafee13790093ee162e43a6be4fda29a05eb9
blanketmanatee <ashleyl.welch@gmail.com>
2020-12-11 05:02:12 +0000

    pep

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,8 +2,8 @@
 from calculator_1 import add, sub, mul, div
 
 if __name == "__main__":
-a = 10
+    a = 10
 b = 5
 d = {"+": add, "-": sub, "*": mul, "/": div}
 for op in "+-*/":
-print("(:d} {:s} {:d} = {:d}".format(a, op, b, d[op](a, b)))
+    print("(:d} {:s} {:d} = {:d}".format(a, op, b, d[op](a, b)))
