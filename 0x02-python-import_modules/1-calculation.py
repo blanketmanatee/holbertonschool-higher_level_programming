6addb5edaaa98fa995565f4d392500722d106e46
blanketmanatee <ashleyl.welch@gmail.com>
2020-12-11 04:59:22 +0000

    initial commit

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name == "__main__":
+a = 10
+b = 5
+d = {"+": add, "-": sub, "*": mul, "/": div}
+for op in "+-*/":
+print("(:d} {:s} {:d} = {:d}".format(a, op, b, d[op](a, b)))
