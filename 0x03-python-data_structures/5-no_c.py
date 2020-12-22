4208a7f5edcf559858cd9de76d2f632c538c26bb
blanketmanatee <ashleyl.welch@gmail.com>
2020-12-22 23:40:45 +0000

    corrections

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
-        new_str = ''
-        for i in my_string:
-            if i == 'C' and i == 'c':
-                return new_str
-    return my_string
+    return (my_string.translate({ord("C"): None, ord("c"): None}))
