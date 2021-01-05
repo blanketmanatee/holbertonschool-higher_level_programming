afd653349e50a99af5172d5088c692aadfeee778
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-05 03:32:49 +0000

    checking the checker

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    printer = 0
+
+    for x in range(x):
+        try:
+            if not isinstance(my_list[x], int):
+                raise ValueError
+            print("{:d}".format(my_list[x]), end='')
+            printer = printer + 1
+        except ValueError:
+            pass
+    print("")
+    return printer
