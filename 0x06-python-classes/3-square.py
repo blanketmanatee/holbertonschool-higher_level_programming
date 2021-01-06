c7cbfe85569db38270da31c7a7170a7b461ddc7f
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-06 05:22:05 +0000

    initial commit no docs yet

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 class Square:
 
     def __init__(self, size=0):
@@ -11,3 +9,6 @@ class Square:
                 self.__size = size
         else:
                 raise TypeError('size must be an integer')
+
+    def area(self):
+        return self.area
