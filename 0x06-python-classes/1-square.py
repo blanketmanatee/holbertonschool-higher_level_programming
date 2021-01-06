e2f11de520e9f729c5f24db6eaa8e10b9b5ce727
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-06 17:14:48 +0000

    docs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
+"""class Square that defines size"""
 
 class Square:
+    """This is a square"""
     def __init__(self, size):
+    """defining size"""
         self.__size = size
+        """private attribute size"""
