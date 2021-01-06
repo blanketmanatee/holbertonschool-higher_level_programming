31bfb08af19171ac9e2346092c6b7f187ec7ab16
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-06 04:10:57 +0000

    fixing docs again

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""This module contains an empty class to practice class declarations""
+""This module contains an empty class to practice class declarations
+""
 
 
 class Square:
-    ""This is my empty class""
-pass
+    ""This is my empty class
+    ""
+    pass
