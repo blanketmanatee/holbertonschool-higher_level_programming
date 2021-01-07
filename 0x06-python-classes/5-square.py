44a813878ad2b71d5dd0196e16b56e88b69dfa3c
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-07 02:34:13 +0000

    stuff

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
+""" class square gets area getter and setter """
+
+
 class Square:
+    """class square"""
 
     def __init__(self, size=0):
-        if type(size) is int:
-            if size < 0:
+        """size and words"""
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """finding size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """validates size"""
+
+        if type(value) is int:
+            if value < 0:
                 raise ValueError('size must be >= 0')
             else:
-                self.__size = size
+                self.__size = value
         else:
                 raise TypeError('size must be an integer')
 
     def area(self):
-        return self.__size **2
+        """area and words"""
+        return self.__size ** 2
