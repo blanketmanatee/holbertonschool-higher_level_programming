14a563f73359c0630758d02169a684a14570fec6
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-07 02:09:08 +0000

    docs

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,17 +3,11 @@
 
 class Square:
     """class square"""
+
     def __init__(self, size=0):
         """size and words"""
-        self.__size = size
 
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-        else:
-                raise TypeError('size must be an integer')
+        self.__size = size
 
     @property
     def size(self):
