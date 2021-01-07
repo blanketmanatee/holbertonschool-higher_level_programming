34d0ccc1ca5ca11188587ed3ade93ea022a9eb0b
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-07 03:03:03 +0000

    checking checker

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,14 +1,58 @@
 #!/usr/bin/python3
-class Square:
+""" class square gets area getter and setter """
 
-    def __init__(self, size=0):
-        if type(size) is int:
-            if size < 0:
+
+class Square:
+    """class square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """size and words"""
+
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """finding size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """validates size"""
+
+        if type(value) is int:
+            if value < 0:
                 raise ValueError('size must be >= 0')
             else:
-                self.__size = size
+                self.__size = value
         else:
                 raise TypeError('size must be an integer')
 
     def area(self):
-        return self.__size **2
+        """area and words"""
+        return self.__size ** 2
+
+    @property
+    def position(self):
+        """ position """
+        return self.__position
+    @position.setter
+    def position(self, value):
+        """ setting position """
+        if type(position) != tuple or len(value != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(position[0], int) is False or position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(position[1], int) is False or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        """prints square with #"""
+        if self.__size == 0:
+            print()
+            return
+        for x in range(self.__size):
+            print("#" * self.__size)
+        return
