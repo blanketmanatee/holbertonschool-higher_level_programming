6de989be3f17c7d79306120e4a27f06eaefd9249
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-07 02:05:20 +0000

    adjusting docs

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-"""introducing getter and setter"""
 
 
 class Square:
     """class square"""
     def __init__(self, size=0):
         """size and words"""
+        self.__size = size
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+                raise TypeError('size must be an integer')
+
+    @property
+    def size(self):
+        """finding size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """validates size"""
         if type(size) is int:
             if size < 0:
                 raise ValueError('size must be >= 0')
@@ -17,12 +34,3 @@ class Square:
     def area(self):
         """area and words"""
         return self.__size **2
-
-    @property
-    def size(self):
-        """finding size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """validates size"""
