0094ec96885776580dbb0cd84437991ed713507b
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-12 23:29:12 -0600

    need to finish task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+""" Addition """
 
 
-def add_integer(a, b=98):
+def add_integer(a, b):
+""" 
+A and B must be ints or floats
+otherwise TypeError raise. 
+A and B must be casted if they are floats.
+Returns an int.
+"""
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return (a + b)
