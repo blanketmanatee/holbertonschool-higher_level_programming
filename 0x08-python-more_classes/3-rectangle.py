d3ca7d3ab5fd54155f421d469e87db1d30f26cd2
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-13 23:09:37 -0600

    docs

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -48,7 +48,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * self.__width) + (2 * self.__height)
+    
     def __str__(self):
+        """ returns printed rectangle """
         shape = ""
         if self.__width == 0 or self.__height == 0:
             return shape
