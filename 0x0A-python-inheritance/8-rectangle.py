38594cddb928330e9769475da255fb7ba58e6da2
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-21 19:57:50 -0600

    fixed typo

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ importing """
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
