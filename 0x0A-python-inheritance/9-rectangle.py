afcbce5eb97a144ac0d7a5d415ece248936a94db
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-21 21:50:17 -0600

    FIXED TABS git push

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,7 +4,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ inherits from BaseGeometry
+     """ inherits from BaseGeometry
     instantation with width and height
     def: __init__(self, width, height):
     width and height must be private w no
@@ -13,11 +13,11 @@ class Rectangle(BaseGeometry):
     print() should print and str() should return
     [Rectangle] <width>/<height> """
      
-def __init__(self, width, height):
-     super().integer_validator("width", width)
-     self.__width = width
-     super().integer_validator("height", height)
-     self.__height = height
+     def __init__(self, width, height):
+          super().integer_validator("width", width)
+          self.__width = width
+          super().integer_validator("height", height)
+          self.__height = height
 
      def __str__(self):
           """ return description of rectangle """
