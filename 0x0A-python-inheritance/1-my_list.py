f45a3401da20566fa1b7046450a0950af1f8164e
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-21 18:26:28 -0600

    fixed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ list in order """
 
-class MyList:
+class MyList(list):
     """MyList class""" 
     
-def print_sorted(self):
-    """prints list in ascending order"""
-    sorted = self.copy()
-    sorted.sort()
-    print(sorted)
+    def print_sorted(self):
+        """prints list in ascending order"""
+        sorted = self.copy()
+        sorted.sort()
+        print(sorted)
