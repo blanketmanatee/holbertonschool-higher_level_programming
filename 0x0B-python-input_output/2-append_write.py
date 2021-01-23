9cbc6c82eddec1bf6432d68275c20d4e907f008e
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-22 19:04:23 -0600

    not counting

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding="UTF-8") as newfile:
+        newfile.write(text)
+    for letters in text:
+        count +=
+    return letters
