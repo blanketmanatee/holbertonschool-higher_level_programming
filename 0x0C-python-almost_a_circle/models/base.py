999626e4e7818f5d924c9a7ec4ceca1c1da221b0
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-25 17:26:49 -0600

    initial commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" base of all classes for this project """
+
+class Base:
+    """ class with private class attribute nb_objects """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Square class """
+Rectangle = __import__('rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square inherits class Rectangle """
