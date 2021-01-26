b078e2e1645788ed25e5c2c8455d9db8a8c06b5f
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-25 19:02:55 -0600

    added README and some of task 2

--- 0x0C-python-almost_a_circle/models/Rectangle.py
+++ 0x0C-python-almost_a_circle/models/Rectangle.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
+""" Class Rectangle inherits from Base """
+Base = __import__('Base').Base
 
 
 class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().id 
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        def width(self):
+
+        @width.setter(self):
+
+        def height(self):
+
+        @height.setter(self):
+
+        def x(self):
+        
+        @x.setter(self):
+
+        def y(self):
+        
+        @y.setter(self):
+
