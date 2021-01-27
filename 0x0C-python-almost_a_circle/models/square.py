780bd7fcb3eeff39627de3df94a05feb70ba04f9
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-26 23:57:00 -0600

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,20 @@ class Square(Rectangle):
         s = "[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.x, self.y, self.size)
         return s
+
+    @property
+    def size(self):
+        """ finds size """
+        return self.__width
+    
+    @size.setter
+    def size(self, value):
+        """ validates size """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+
+    
