b949ebe63c151e523dc943e35d3399e663ba7d8d
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-27 00:52:09 -0600

    pep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,9 +2,10 @@
 """ Class Rectangle inherits from Base """
 from models.base import Base
 
+
 class Rectangle(Base):
     """ class Rectangle inherits base """
-    def __init__(self, width, height, x=0, y=0, id=None): 
+    def __init__(self, width, height, x=0, y=0, id=None):
         """ initialize instance """
         self.width = width
         self.height = height
@@ -21,9 +22,9 @@ class Rectangle(Base):
     def width(self, value):
         """ validating width """
         if type(value) != int:
-            raise TypeError ("width must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError ("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -35,16 +36,16 @@ class Rectangle(Base):
     def height(self, value):
         """ validating height """
         if type(value) != int:
-            raise TypeError ("height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError ("height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
         """ finding x """
         return self.__x
-        
+
     @x.setter
     def x(self, value):
         """ validating x """
