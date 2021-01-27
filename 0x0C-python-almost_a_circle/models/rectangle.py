61946c3b0a3242410bbd133d7e8eb2e42f04c533
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-27 00:49:44 -0600

    trying to fix the case issue

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,9 @@
 from models.base import Base
 
 class Rectangle(Base):
+    """ class Rectangle inherits base """
     def __init__(self, width, height, x=0, y=0, id=None): 
+        """ initialize instance """
         self.width = width
         self.height = height
         self.x = x
@@ -12,10 +14,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ finding width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ validating width """
         if type(value) != int:
             raise TypeError ("width must be an integer")
         if value <= 0:
@@ -24,10 +28,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ finding height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ validating height """
         if type(value) != int:
             raise TypeError ("height must be an integer")
         if value <= 0:
@@ -36,10 +42,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ finding x """
         return self.__x
         
     @x.setter
     def x(self, value):
+        """ validating x """
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -48,10 +56,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ finding y """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ validating y """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -59,9 +69,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """ area """
         return self.width * self.height
 
     def display(self):
+        """ printing the shape """
         if self.width == 0 or self.height == 0:
             print("")
             return
@@ -76,9 +88,12 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.x, self.y, self.width, self.height)
+        """ describing the shape """
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format
+        (self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
+        """ adding args and kwargs """
         if len(args) != 0:
             i = 0
             attr = ['id', 'width', 'height', 'x', 'y']
@@ -88,4 +103,3 @@ class Rectangle(Base):
         else:
             for key, val in kwargs.items():
                 setattr(self, key, val)
-
