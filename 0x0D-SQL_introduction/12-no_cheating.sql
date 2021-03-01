16e8fd2bad69907ea00b75e37ca9e52a655a20bc
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-01 14:42:54 -0600

    initial push

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- update Bob's score only using his name
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
 -- creates a table called first_table
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John' 10), (2, 'Alex' 3), (3, 'Bob', 14), (4, 'George' 8);
