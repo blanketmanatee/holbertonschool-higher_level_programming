a44e033a26439e5e5baf5f910c274c66be2f0295
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-01 21:14:38 -0600

    updating utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,3 @@
 -- convert to utf8 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
-ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
