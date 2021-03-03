ba624604abb4658fc1209a2a347efd2c6df4d385
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-02 20:32:07 -0600

    initial push

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- creates database hbtn_0d_usa and table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS 'states' (id INT DEFAULT 1, UNIQUE (ID), name VARCHAR(256));
