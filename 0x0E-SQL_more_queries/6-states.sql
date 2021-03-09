f9b097f4db36dc5009649547a7da7f19486afc6e
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-09 00:57:15 -0600

    checker

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- creates database hbtn_0d_usa and table states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT DEFAULT 1, UNIQUE (ID), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa.states' (id INT DEFAULT 1, UNIQUE (ID), name VARCHAR(256));
