2b5cc24b3aef3cd3430efd9363f0a6a1e1f0f097
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-09 22:30:35 -0600

    doc

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """ appends a string at the end of text file """
 
 def append_write(filename="", text=""):
+    """ append a string """
     with open(filename, 'a', encoding="UTF-8") as newfile:
         newfile.write(text)
     for letters in text:
