5c5d91099cca3789bb1f3719ab34947ba66d7ee5
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-10 02:05:06 -0600

    size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -71,3 +71,14 @@ class Rectangle:
         """ deleting instance """
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_2.area() > rect_1.area():
+            return rect_2
+        return rect_1
+    
+    def square(cls, size=0):
+        return cls(size, size)
