f534c994128745b3820846917c8fc726d0503b52
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-11 22:22:09 -0600

    fixed typo

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 -- display in ascending order by title and genre id
 -- if no genre display NULL
 
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
