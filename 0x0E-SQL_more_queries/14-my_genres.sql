bd9bab0083f92db05aabe45fdfc8bb0c7a50162f
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-11 22:28:10 -0600

    genre id

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- list all genres for the show Dexter
-SELECT tv_genres.name FROM tv_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.show_id WHERE tv_shows.title = "DEXTER" ORDER BY tv_genres.name;
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.show_id WHERE tv_shows.title = "DEXTER" ORDER BY tv_genres.name;
