8f6665efc5584af73b821a07199e3eeb92cfb4c9
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-28 04:25:29 +0000

    initial push

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+import mySQLdb
+
+if __name == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT *  from 'states' WHERE BINARY 'name' = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
