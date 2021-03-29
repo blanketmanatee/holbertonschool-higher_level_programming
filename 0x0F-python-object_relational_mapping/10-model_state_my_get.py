de318f87a3125b414250fe12c2d54686fd9676cb
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-29 15:59:00 +0000

    changed all and removed extra space

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sys import argv
     Session = sessionmaker()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'. format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
     Base.metadata.create_all(engine)
-    instances = session.query(State).filter(State.name == argv[4]).all()
+    instances = session.query(State).filter(State.name == argv[4]).first()
     if instances:
         for instance in instances:
             if instance.name == argv[4]:
