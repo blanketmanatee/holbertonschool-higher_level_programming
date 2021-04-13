28755c3a45fb0fc6d1ae866d27797efeae0143bf
blanketmanatee <ashleyl.welch@gmail.com>
2021-04-13 02:47:29 +0000

    Fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    r = requests.port(url, data=value)
+    r = requests.post(url, data=value)
     print(r.text)
