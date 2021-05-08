7fce169c8b6e77a6bc2797a57b2c236c5572fbc6
blanketmanatee <ashleyl.welch@gmail.com>
2021-05-08 05:36:00 +0000

    initial push

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+require('request').get(process.argv[2], function (err, r, body) {
+    if (err) {
+	console.log(err);
+	} else {
+	    let counter = 0;
+	    let data = JSON.parse(body).results;
+	    for (let i = 0; i < data.length; i++) {
+		if (data[i].characters[n].includes('/18/')) {
+		    counter++;
+		    break;
+		    }
+		}
+	    }
+    console.log(counter);
+    }
+});
